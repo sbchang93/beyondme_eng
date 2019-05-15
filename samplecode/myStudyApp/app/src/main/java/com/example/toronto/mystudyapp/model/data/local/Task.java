174bf92af9a8415ee7755966981de4a41ce285f7
sbchang93 <32184131+sbchang93@users.noreply.github.com>
2019-05-16 08:58:17 +0900

    Delete Task.java

--- samplecode/myStudyApp/app/src/main/java/com/example/toronto/mystudyapp/model/data/local/Task.java
+++
@@ -1,9 +0,0 @@
-package com.example.toronto.mystudyapp.model.data.local;
-
-public class Task  {
-    public final String mTask;
-
-    public Task(String task) {
-        this.mTask = task;
-    }
-}
