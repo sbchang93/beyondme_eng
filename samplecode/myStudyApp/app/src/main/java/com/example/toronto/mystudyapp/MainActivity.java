29c98c08134608cde7ecb2960742355a8a8ce48a
sbchang93 <32184131+sbchang93@users.noreply.github.com>
2019-10-30 19:47:02 +0900

    Add files via upload

--- samplecode/myStudyApp/app/src/main/java/com/example/toronto/mystudyapp/MainActivity.java
+++ samplecode/myStudyApp/app/src/main/java/com/example/toronto/mystudyapp/MainActivity.java
@@ -8,6 +8,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
+import com.example.toronto.mystudyapp.util.Logger;
 import com.example.toronto.mystudyapp.view.AudioActivity;
 import com.example.toronto.mystudyapp.view.CallOtherActivity;
 import com.example.toronto.mystudyapp.view.DataBindingTest01Activity;
@@ -35,6 +36,8 @@ import com.example.toronto.mystudyapp.view.LocationManagerActivity;
 import com.example.toronto.mystudyapp.view.NameActivityForViewModel;
 import com.example.toronto.mystudyapp.view.ObserverActivity;
 import com.example.toronto.mystudyapp.view.ObserverMainActivity;
+import com.example.toronto.mystudyapp.view.RecyclerView2Activity;
+import com.example.toronto.mystudyapp.view.RecyclerView3Activity;
 import com.example.toronto.mystudyapp.view.RecyclerViewActivity;
 import com.example.toronto.mystudyapp.view.ReflectionActivity;
 import com.example.toronto.mystudyapp.view.SaveState02Activity;
@@ -45,53 +48,90 @@ import com.example.toronto.mystudyapp.view.TextViewActivity;
 import com.example.toronto.mystudyapp.view.UserActivity;
 import com.example.toronto.mystudyapp.view.VideoActivity;
 import com.example.toronto.mystudyapp.view.ViewActivity;
+import com.example.toronto.mystudyapp.view.ViewPagerActivity;
+import com.example.toronto.mystudyapp.view.ViewPagerCustomActivity;
 
+import java.util.HashMap;
 import java.util.concurrent.TimeUnit;
 
 import io.reactivex.Observable;
 
 public class MainActivity extends AppCompatActivity {
+    private final String TAG = this.getClass().getSimpleName();
+    private HashMap<Integer, String> sampleNameMap = new HashMap();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Observable.just("")
+                .delay(1, TimeUnit.SECONDS)
+                .subscribe(s -> {
+                    // runOnUiThread : https://itmining.tistory.com/6
+                    runOnUiThread(new Runnable() {
+                        public void run() {
+                            Toast.makeText(MainActivity.this, "An Observable run once in MainActivity", Toast.LENGTH_LONG).show();
+                        }
+                    });
+//                    startActivity(new Intent(this, FragmentLayout.class));
+//                    finish();
+                });
+
+        //Handler
         Handler handle = new Handler();
         Runnable callback = new Runnable() {
             public void run() {
                 //startActivity(new Intent(MainActivity.this, ListViewActivity.class));
-                //Toast.makeText(MainActivity.this, "handler in MainActivity" , Toast.LENGTH_SHORT).show();
+                Toast.makeText(MainActivity.this, "An handler run once in MainActivity" , Toast.LENGTH_LONG).show();
             }
         };
         handle.post(callback);
 
-        findViewById(R.id.normalSamples).setOnClickListener(mClickListener);
-        findViewById(R.id.dbSamples).setOnClickListener(mClickListener);
-        findViewById(R.id.observalbeSamples).setOnClickListener(mClickListener);
-        findViewById(R.id.listMenuSamples).setOnClickListener(mClickListener);
-        findViewById(R.id.drawSamples).setOnClickListener(mClickListener);
-    }
+        findViewById(R.id.normal_samples).setOnClickListener(mClickListener);
+        findViewById(R.id.db_samples).setOnClickListener(mClickListener);
+        findViewById(R.id.observalbe_samples).setOnClickListener(mClickListener);
+        findViewById(R.id.list_menu_samples).setOnClickListener(mClickListener);
+        findViewById(R.id.draw_samples).setOnClickListener(mClickListener);
+        findViewById(R.id.recycler_view_samples).setOnClickListener(mClickListener);
 
+        // ------------------------------------------------------------------------------------------------
+        // For Debugging Message
+        sampleNameMap.put(R.id.normal_samples, "R.id.normal_samples");
+        sampleNameMap.put(R.id.db_samples, "R.id.db_samples");
+        sampleNameMap.put(R.id.observalbe_samples, "R.id.observalbe_samples");
+        sampleNameMap.put(R.id.list_menu_samples, "R.id.list_menu_samples");
+        sampleNameMap.put(R.id.draw_samples, "R.id.draw_samples");
+        sampleNameMap.put(R.id.recycler_view_samples, "R.id.recycler_view_samples");
+
+    }
 
     Button.OnClickListener mClickListener = new View.OnClickListener() {
         public void onClick(View v) {
+            Logger.d(TAG, "mClickListener : " + sampleNameMap.get(v.getId()));
+
             switch(v.getId()) {
-                case R.id.normalSamples:
+                case R.id.normal_samples:
                     launchSampleActivities();
                     break;
-                case R.id.dbSamples:
+                case R.id.db_samples:
                     launchRoomDBSamples();
                     break;
-                case R.id.observalbeSamples:
+                case R.id.observalbe_samples:
                     launchObservableSamples();
                     break;
-                case R.id.listMenuSamples:
+                case R.id.list_menu_samples:
                     listMenusSamples();
                     break;
-                case R.id.drawSamples:
+                case R.id.draw_samples:
                     drawSamplesSamples();
                     break;
+
+                case R.id.recycler_view_samples:
+                    recyclerViewSamples();
+                    break;
+
+
 
                  // Remember ===> findViewById(R.id.normalSamples).setOnClickListener(mClickListener);
             }
@@ -106,29 +146,35 @@ public class MainActivity extends AppCompatActivity {
                     //startActivity(new Intent(this, LiveDataActivity.class));
                     //startActivity(new Intent(this, ObserverActivity.class));
 
-                    startActivity(new Intent(this, CallOtherActivity.class));
-                    startActivity(new Intent(this, HashMapActivity.class));
-                    startActivity(new Intent(this, ViewActivity.class));
-                    startActivity(new Intent(this, HandlerActivity.class));
-                    startActivity(new Intent(this, LocationManagerActivity.class));
-                    startActivity(new Intent(this, TextViewActivity.class));
-                    startActivity(new Intent(this, AudioActivity.class));
-                    startActivity(new Intent(this, DialogActivity.class));
-                    startActivity(new Intent(this, Dialog2Activity.class));
-                    startActivity(new Intent(this, Dialog3Activity.class));
-                    startActivity(new Intent(this, Dialog4Activity.class));
-                    startActivity(new Intent(this, Dialog5Activity.class));
-                    startActivity(new Intent(this, Test01Activity.class));
-                    startActivity(new Intent(this, Test02_MyViewActivity.class));
-                    startActivity(new Intent(this, SaveState02Activity.class));
-                    startActivity(new Intent(this, SaveState03Activity.class));
-                    startActivity(new Intent(this, DialogTest01Activity.class));
-                    startActivity(new Intent(this, DialogTest02Activity.class));
-                    startActivity(new Intent(this, DialogTest03Activity.class));
-                    startActivity(new Intent(this, DialogTest04Activity.class));
-                    startActivity(new Intent(this, DataBindingTest01Activity.class));
-                    startActivity(new Intent(this, NameActivityForViewModel.class));
-                    finish();
+//                    startActivity(new Intent(this, CallOtherActivity.class));
+//                    startActivity(new Intent(this, HashMapActivity.class));
+//                    startActivity(new Intent(this, ViewActivity.class));
+//                    startActivity(new Intent(this, HandlerActivity.class));
+//                    startActivity(new Intent(this, LocationManagerActivity.class));
+//                    startActivity(new Intent(this, TextViewActivity.class));
+//                    startActivity(new Intent(this, AudioActivity.class));
+//                    startActivity(new Intent(this, DialogActivity.class));
+//                    startActivity(new Intent(this, Dialog2Activity.class));
+//                    startActivity(new Intent(this, Dialog3Activity.class));
+//                    startActivity(new Intent(this, Dialog4Activity.class));
+//                    startActivity(new Intent(this, Dialog5Activity.class));
+//                    startActivity(new Intent(this, Test01Activity.class));
+//                    startActivity(new Intent(this, Test02_MyViewActivity.class));
+//                    startActivity(new Intent(this, SaveState02Activity.class));
+//                    startActivity(new Intent(this, SaveState03Activity.class));
+//                    startActivity(new Intent(this, DialogTest01Activity.class));
+//                    startActivity(new Intent(this, DialogTest02Activity.class));
+//                    startActivity(new Intent(this, DialogTest03Activity.class));
+//                    startActivity(new Intent(this, DialogTest04Activity.class));
+//                    startActivity(new Intent(this, DataBindingTest01Activity.class));
+//                    startActivity(new Intent(this, NameActivityForViewModel.class));
+
+                    startActivity(new Intent(this, ViewPagerActivity.class));
+                    //startActivity(new Intent(this, ViewPagerCustomActivity.class));
+
+//                    finish();
+
+
                 });
     }
 
@@ -138,7 +184,7 @@ public class MainActivity extends AppCompatActivity {
                 .delay(1, TimeUnit.MILLISECONDS)
                 .subscribe(s -> {
                     startActivity(new Intent(this, UserActivity.class));
-                    finish();
+//                    finish();
                 });
     }
 
@@ -156,7 +202,7 @@ public class MainActivity extends AppCompatActivity {
 
 //                    startActivity(new Intent(this, LiveDataActivity.class));
 //                    startActivity(new Intent(this, ObserverActivity.class));
-                    finish();
+//                    finish();
                 });
     }
 
@@ -171,8 +217,7 @@ public class MainActivity extends AppCompatActivity {
                     startActivity(new Intent(this, ListView2Activity.class));
                     startActivity(new Intent(this, ListView02CustomActivity.class));
                     startActivity(new Intent(this, ListView01Activity.class));
-
-                    finish();
+//                    finish();
                 });
     }
 
@@ -186,11 +231,20 @@ public class MainActivity extends AppCompatActivity {
                     startActivity(new Intent(this, VideoActivity.class));
                     startActivity(new Intent(this, DrawTestActivity.class));
                     startActivity(new Intent(this, ReflectionActivity.class));
-
-                    finish();
+//                    finish();
                 });
     }
 
+    private void recyclerViewSamples() {
+        Observable.just("")
+                .delay(1, TimeUnit.MILLISECONDS)
+                .subscribe(s -> {
+                    //startActivity(new Intent(this, RecyclerView2Activity.class));
+                    startActivity(new Intent(this, RecyclerView3Activity.class));
+
+//                    finish();
+                });
+    }
 
 }
 
