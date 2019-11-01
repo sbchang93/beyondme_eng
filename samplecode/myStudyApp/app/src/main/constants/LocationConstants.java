299ebe306e0f6de99710e384fbfe32e03d50b5c5
sbchang93 <32184131+sbchang93@users.noreply.github.com>
2019-11-01 10:08:27 +0900

    Delete LocationConstants.java

--- samplecode/myStudyApp/app/src/main/constants/LocationConstants.java
+++
@@ -1,8 +0,0 @@
-package com.example.toronto.mystudyapp.constants;
-
-public class LocationConstants {
-    public static final String EXTRA_GPS_ACCURACY = "extra_gps_accuracy";
-    public static final String EXTRA_GPS_LONGITUDE = "extra_gps_longitude";
-    public static final String EXTRA_GPS_LATITUDE = "extra_gps_latitude";
-
-}
