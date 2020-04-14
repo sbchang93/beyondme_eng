625dbd6fef3df22d62f7df4ee51cbf8fb2834882
sbchang93 <sbchang93@gmail.com>
2020-04-14 23:22:46 +0900

    라이브러리 테스트 모듈 소스 변경

--- samplecode_test/module_library_test/TestModule003/app/src/main/java/com/happybom/testmodule003/MyTest.java
+++ samplecode_test/module_library_test/TestModule003/app/src/main/java/com/happybom/testmodule003/MyTest.java
@@ -6,6 +6,7 @@ import android.widget.Toast;
 
 public class MyTest{
     public void showMyToast(Context context, Resources res){
-        Toast.makeText(context, res.getString(R.string.warning), Toast.LENGTH_SHORT);
+        //Toast.makeText(context, res.getString(R.string.warning), Toast.LENGTH_SHORT);
+        Toast.makeText(context, "Hello, my Module 003", Toast.LENGTH_LONG);
     }
 }
