cf14b6881105b999f9a19b36696dc5f86d00ff26
sbchang93 <sbchang93@gmail.com>
2020-04-18 23:10:28 +0900

    UIPlatformFlower002-001 (2020.04.18) : change Red App and Blue App to library   and then add them into Main Application Module   and run it together

---
+++ samplecode_test/module_library_test/UIPlatformFlower002-001/app/src/main/java/com/happybom/uiplatformflower002/MainActivity.java
@@ -0,0 +1,54 @@
+package com.happybom.uiplatformflower002;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import com.happybom.blueapp.BlueApp;
+import com.happybom.commonlibrary.BaseMainActivity;
+import com.happybom.redapp.RedApp;
+import com.happybom.uiplatformflower002.R;
+import com.happybom.uiplatformflower002.ui.runScreen02Activity;
+import com.happybom.uiplatformflower002.view.common.UiFlowerBaseActivity;
+
+import java.util.concurrent.TimeUnit;
+
+import io.reactivex.Observable;
+
+public class MainActivity extends UiFlowerBaseActivity {
+    private static final String TAG = "MainActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+
+        //RedApp redApp = Injector.provideRedApp(this);
+
+        Observable.just("RedApp")
+                .delay(1, TimeUnit.SECONDS)
+                .subscribe(s -> {
+                    RedApp redApp = Injector.provideRedApp(this);
+                    redApp.startApp(this);
+
+                    //startActivity(new Intent(this, runScreen02Activity.class));
+                    //finish();
+                }, t ->{});
+
+        Observable.just("BlueApp")
+                .delay(2, TimeUnit.SECONDS)
+                .subscribe(s -> {
+                    BlueApp blueApp = Injector.provideBlueApp(this);
+                    blueApp.startApp(this);
+                }, t ->{});
+
+
+        //        MyTest str = new MyTest();
+        //        str.showMyToast(getApplicationContext(), getResources());
+
+    }
+}
---
+++ samplecode_test/module_library_test/UIPlatformFlower002-001/blueapp/src/main/java/com/happybom/blueapp/BlueMainActivity.java
@@ -0,0 +1,27 @@
+package com.happybom.blueapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.content.ContextCompat;
+
+import android.os.Bundle;
+
+import com.happybom.commonlibrary.AppManager;
+import com.happybom.commonlibrary.BaseMainActivity;
+
+// Reference Homepage for common library creation
+// https://kokohapps.tistory.com/entry/Android-Module-%EC%9D%B4%EC%9A%A9%ED%95%98%EC%97%AC-%EB%B9%84%EC%8A%B7%ED%95%9C-%EC%95%B1-%EC%97%AC%EB%9F%AC%EA%B0%9C-%EC%B0%8D%EC%96%B4%EB%82%B4%EA%B8%B0
+public class BlueMainActivity extends BaseMainActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        AppManager.appType = AppManager.AppType.BlueApp;
+        AppManager.themeColor = ContextCompat.getColor(this, R.color.colorPrimary);
+
+        this.button.setTextColor(AppManager.themeColor);
+        this.button.setText("Blue Button");
+
+        this.textView.setText("This is Blue Apk");
+    }
+}
