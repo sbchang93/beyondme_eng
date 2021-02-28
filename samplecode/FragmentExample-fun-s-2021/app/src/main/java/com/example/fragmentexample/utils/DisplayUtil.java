6f781f93d4fea3bb6e990eecaf610cdb308008d1
sbchang93 <sbchang93@gmail.com>
2021-02-28 21:47:47 +0900

    dp - px 관련

--- samplecode/FragmentExample-fun-s-2021/app/src/main/java/com/example/fragmentexample/utils/DisplayUtil.java
+++ samplecode/FragmentExample-fun-s-2021/app/src/main/java/com/example/fragmentexample/utils/DisplayUtil.java
@@ -1,22 +1,61 @@
 package com.example.fragmentexample.utils;
 
+
 import android.content.Context;
 import android.util.DisplayMetrics;
+import android.util.TypedValue;
+import android.view.WindowManager;
 
 public class DisplayUtil {
 
     // https://stackoverflow.com/questions/16103344/android-text-size-on-canvas-differ-from-text-size-in-textview
-    public static float convertPixelsToDp(Context context, float px){
+    public static float convertPixelsToDp(Context context, float px) {
         DisplayMetrics metrics = context.getResources().getDisplayMetrics();
         float dp = px / (metrics.densityDpi / 160f);
         return dp;
 
     }
 
-    public static float convertDpToPixel(Context context, float dp){
+    public static float convertDpToPixel(Context context, float dp) {
         DisplayMetrics metrics = context.getResources().getDisplayMetrics();
-        float px = dp * (metrics.densityDpi/160f);
+        float px = dp * (metrics.densityDpi / 160f);
         return px;
 
     }
+
+    // https://inma.tistory.com/72
+    public static void testSample(Context context) {
+        DisplayMetrics displayMetrics = new DisplayMetrics();
+        WindowManager windowManager = (WindowManager) context.getSystemService(Context.WINDOW_SERVICE);
+        windowManager.getDefaultDisplay().getMetrics(displayMetrics);
+
+        int dpi = displayMetrics.densityDpi;
+        float density = displayMetrics.density;
+    }
+
+    // https://inma.tistory.com/72
+    public static int dpToPx(Context context, float dp) {
+        int px = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dp, context.getResources().getDisplayMetrics());
+        return px;
+    }
+
+//    public float dpToPx(Context context, float dp) {
+//        DisplayMetrics dm = context.getResources().getDisplayMetrics();
+//        return (int)TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, dp, dm);
+//    }
+
+    // https://inma.tistory.com/72
+    public static float pxToDp(Context context, float px) {
+        float density = context.getResources().getDisplayMetrics().density;
+
+        if (density == 1.0)  // mdpi (160dpi) -- xxxhdpi (density = 4) 기준으로 density 값을 재설정 한다.
+            density *= 4.0;
+        else if (density == 1.5)  // hdpi (240dpi)
+            density *= (8 / 3);
+        else if (density == 2.0)  // xhdpi (320dpi)
+            density *= 2.0;
+
+        return px / density;
+    }
 }
+
--- samplecode/FragmentExample-fun-s-2021/app/src/main/java/com/example/fragmentexample/view/cardmatchgame/CardGameView.java
+++ samplecode/FragmentExample-fun-s-2021/app/src/main/java/com/example/fragmentexample/view/cardmatchgame/CardGameView.java
@@ -1,30 +1,56 @@
 package com.example.fragmentexample.view.cardmatchgame;
 
+
 import android.content.Context;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Rect;
+import android.view.MotionEvent;
 import android.view.View;
 
 import com.example.fragmentexample.R;
 import com.example.fragmentexample.model.cardmatchgame.Card;
+import com.example.fragmentexample.utils.DisplayUtil;
 
 public class CardGameView extends View {
-    Bitmap mBackgroundImage;
-    Bitmap mBacksideCard;
-    Bitmap mCardRed;
-    Bitmap mCardBlue;
-    Bitmap mCardGreen;
+    public static final int STATE_READY = 0;
+    public static final int STATE_GAME = 1;
+    public static final int STATE_END = 2;
+    private int mState = STATE_READY;
+
+    Bitmap mBackgroundImage = BitmapFactory.decodeResource(getResources(), R.drawable.background, null);
+    Bitmap mCardBackside = BitmapFactory.decodeResource(getResources(), R.drawable.backside, null);
+    Bitmap mCardRed = BitmapFactory.decodeResource(getResources(), R.drawable.front_red, null);
+    Bitmap mCardBlue = BitmapFactory.decodeResource(getResources(), R.drawable.front_blue, null);
+    Bitmap mCardGreen = BitmapFactory.decodeResource(getResources(), R.drawable.front_green, null);
     Card mShuffle[][];
+
+    int _0dp = DisplayUtil.dpToPx(getContext(), 0);
+    int _20dp = DisplayUtil.dpToPx(getContext(), 20);
+    int _35dp = DisplayUtil.dpToPx(getContext(), 35);
+    int _40dp = DisplayUtil.dpToPx(getContext(), 40);
+    int _57dp = DisplayUtil.dpToPx(getContext(), 57);
+    int _90dp = DisplayUtil.dpToPx(getContext(), 90);
+    int _130dp = DisplayUtil.dpToPx(getContext(), 130);
+    int _150dp = DisplayUtil.dpToPx(getContext(), 150);
+
+//    int _0dp = (int) DisplayUtil.convertDpToPixel(getContext(), 0);
+//    int _20dp = (int) DisplayUtil.convertDpToPixel(getContext(), 20);
+//    int _35dp = (int) DisplayUtil.convertDpToPixel(getContext(), 35);
+//    int _40dp = (int) DisplayUtil.convertDpToPixel(getContext(), 40);
+//    int _57dp = (int) DisplayUtil.convertDpToPixel(getContext(), 57);
+//    int _90dp = (int) DisplayUtil.convertDpToPixel(getContext(), 90);
+//    int _130dp = (int) DisplayUtil.convertDpToPixel(getContext(), 130);
+//    int _150dp = (int) DisplayUtil.convertDpToPixel(getContext(), 150);
+
+    private int mX;
+    private int mY;
 
     public CardGameView(Context context) {
         super(context);
-        mBackgroundImage = BitmapFactory.decodeResource(getResources(), R.drawable.background, null);
-        mBacksideCard = BitmapFactory.decodeResource(getResources(), R.drawable.backside, null);
-        mCardRed = BitmapFactory.decodeResource(getResources(), R.drawable.front_red, null);
-        mCardBlue = BitmapFactory.decodeResource(getResources(), R.drawable.front_blue, null);
-        mCardGreen = BitmapFactory.decodeResource(getResources(), R.drawable.front_green, null);
-
         mShuffle = new Card[3][2];
 
         setCardShuffle();
@@ -40,20 +66,45 @@ public class CardGameView extends View {
     }
 
     @Override
+    public boolean onTouchEvent(MotionEvent event) {
+
+        if (mState == STATE_READY) {
+            mState = STATE_GAME;
+        } else if (mState == STATE_GAME) {
+            mX = (int) event.getX();
+            mY = (int) event.getY();
+
+        } else if (mState == STATE_END) {
+            mState = STATE_READY;
+        }
+
+        invalidate();
+        return true;
+        //return super.onTouchEvent(event);
+    }
+
+    @Override
     protected void onDraw(Canvas canvas) {
         canvas.drawBitmap(mBackgroundImage, 0, 0, null);
         for (int y = 0; y < 2; y++) {
             for (int x = 0; x < 3; x++) {
                 if (mShuffle[x][y].mColor == Card.IMG_RED)
-                    canvas.drawBitmap(mCardRed, 80 + x * 150, 400 + y * 200, null);
+                    canvas.drawBitmap(mCardRed, _35dp + x * _90dp, _150dp + y * _130dp, null);
                 else if (mShuffle[x][y].mColor == Card.IMG_GREEN)
-                    canvas.drawBitmap(mCardGreen, 80 + x * 150, 400 + y * 200, null);
+                    canvas.drawBitmap(mCardGreen, _35dp + x * _90dp, _150dp + y * _130dp, null);
                 else if (mShuffle[x][y].mColor == Card.IMG_BLUE)
-                    canvas.drawBitmap(mCardBlue, 80 + x * 150, 400 + y * 200, null);
+                    canvas.drawBitmap(mCardBlue, _35dp + x * _90dp, _150dp + y * _130dp, null);
             }
         }
 
-        //canvas.drawBitmap(mBacksideCard, 80+x*150, 400+y*200, null);
+        Paint p = new Paint();
+        p.setTextSize(_20dp);
+        p.setColor(Color.BLUE);
+        canvas.drawText("이벤트 좌표 X:" + mX + ", Y:" + mY, _0dp, _20dp, p);
+
+        int x_dp = (int) DisplayUtil.convertPixelsToDp(getContext(), mX);
+        int y_dp = (int) DisplayUtil.convertPixelsToDp(getContext(), mY);
+        canvas.drawText("x_dp:" + x_dp + ", y_dp:" + y_dp, _0dp, _20dp*2, p);
     }
 
 
