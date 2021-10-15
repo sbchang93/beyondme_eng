3c7c738e8ff180b08e8af853f60d08ffacbd587d
sbchang93 <32184131+sbchang93@users.noreply.github.com>
2021-10-15 16:29:59 +0900

    Add files via upload

--- samplecode/FragmentExample-fun-s-2021/app/src/main/java/com/example/fragmentexample/view/settings_example/SettingsActivity.java
+++ samplecode/FragmentExample-fun-s-2021/app/src/main/java/com/example/fragmentexample/view/settings_example/SettingsActivity.java
@@ -2,13 +2,25 @@ package com.example.fragmentexample.view.settings_example;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.preference.CheckBoxPreference;
+import androidx.preference.Preference;
+import androidx.preference.PreferenceCategory;
+import androidx.preference.SwitchPreference;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
 
 import com.example.fragmentexample.R;
+import com.example.fragmentexample.utils.PreferenceUtils;
+
+import static com.example.fragmentexample.constants.Constants.emailKey;
+import static com.example.fragmentexample.constants.Constants.memoKey;
+import static com.example.fragmentexample.constants.Constants.nameKey;
+import static com.example.fragmentexample.constants.Constants.phoneKey;
 
 // Reference URLs
 // https://kumgo1d.tistory.com/31
@@ -17,10 +29,36 @@ public class SettingsActivity extends AppCompatActivity {
 
     private static final int SHOW_SETTINGS_PREFERENCE = 1;
 
+    TextView mtvName;
+    TextView mtvPhone;
+    TextView mtvEmail;
+    TextView mtvMemo;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        boolean bChecked = PreferenceUtils.getInstance().getBooleanWithKey(this, nameKey);
+        mtvName = findViewById(R.id.textview_name);
+        mtvName.setVisibility(bChecked ? View.VISIBLE : View.INVISIBLE);
+
+        bChecked = PreferenceUtils.getInstance().getBooleanWithKey(this, phoneKey);
+        mtvPhone = findViewById(R.id.textview_phone);
+        mtvPhone.setVisibility(bChecked ? View.VISIBLE : View.INVISIBLE);
+
+        bChecked = PreferenceUtils.getInstance().getBooleanWithKey(this, emailKey);
+        mtvEmail = findViewById(R.id.textview_email);
+        mtvEmail.setVisibility(bChecked ? View.VISIBLE : View.INVISIBLE);
+
+        bChecked = PreferenceUtils.getInstance().getBooleanWithKey(this, memoKey);
+        mtvMemo = findViewById(R.id.textview_memo);
+        mtvMemo.setVisibility(bChecked ? View.VISIBLE : View.INVISIBLE);
     }
 
     @Override
--- samplecode/FragmentExample-fun-s-2021/app/src/main/java/com/example/fragmentexample/view/settings_example/SettingsPreferenceFragment.java
+++ samplecode/FragmentExample-fun-s-2021/app/src/main/java/com/example/fragmentexample/view/settings_example/SettingsPreferenceFragment.java
@@ -1,14 +1,75 @@
 package com.example.fragmentexample.view.settings_example;
 
+import android.content.Context;
 import android.os.Bundle;
 
+import androidx.preference.Preference;
+import androidx.preference.PreferenceCategory;
 import androidx.preference.PreferenceFragmentCompat;
+import androidx.preference.SwitchPreference;
 
 import com.example.fragmentexample.R;
+import com.example.fragmentexample.utils.PreferenceUtils;
 
-public class SettingsPreferenceFragment extends PreferenceFragmentCompat {
+import static com.example.fragmentexample.constants.Constants.emailKey;
+import static com.example.fragmentexample.constants.Constants.memoKey;
+import static com.example.fragmentexample.constants.Constants.nameKey;
+import static com.example.fragmentexample.constants.Constants.phoneKey;
+
+public class SettingsPreferenceFragment extends PreferenceFragmentCompat implements Preference.OnPreferenceChangeListener {
+    private Context mContext;
+
+    private PreferenceCategory mDisplaySettingsPrefenceCategory = null;
+
+    SwitchPreference spName = null;
+    SwitchPreference spPhone = null;
+    SwitchPreference spEmail = null;
+    SwitchPreference spMemo = null;
+
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+        mContext = getContext();
+
         setPreferencesFromResource(R.xml.setting_preference, rootKey);
+
+        spName = findPreference(nameKey);
+        spName.setOnPreferenceChangeListener(this);
+        spPhone = findPreference(phoneKey);
+        spPhone.setOnPreferenceChangeListener(this);
+        spEmail = findPreference(emailKey);
+        spEmail.setOnPreferenceChangeListener(this);
+        spMemo = findPreference(memoKey);
+        spMemo.setOnPreferenceChangeListener(this);
+    }
+
+    @Override
+    public boolean onPreferenceChange(Preference preference, Object newValue) {
+        String key = preference.getKey();
+
+        if (key.equals(nameKey) || key.equals(phoneKey) || key.equals(emailKey) || key.equals(memoKey) ) {
+            boolean bChecked = Boolean.valueOf(newValue.toString());
+            PreferenceUtils.getInstance().setBooleanWithKey(mContext, key, bChecked);
+            //PreferenceUtils.getInstance().setBooleanWithKey(mContext, key, Boolean.valueOf(newValue.toString()));
+            return true;
+        }
+
+        return false;
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+
+        boolean bChecked = PreferenceUtils.getInstance().getBooleanWithKey(mContext, nameKey);
+        spName.setChecked(bChecked);
+
+        bChecked = PreferenceUtils.getInstance().getBooleanWithKey(mContext, phoneKey);
+        spPhone.setChecked(bChecked);
+
+        bChecked = PreferenceUtils.getInstance().getBooleanWithKey(mContext, emailKey);
+        spEmail.setChecked(bChecked);
+
+        bChecked = PreferenceUtils.getInstance().getBooleanWithKey(mContext, memoKey);
+        spMemo.setChecked(bChecked);
     }
 }
