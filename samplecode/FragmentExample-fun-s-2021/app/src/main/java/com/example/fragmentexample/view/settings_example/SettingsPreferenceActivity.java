bdb98f708efe33024602d7a0405f0928b52491ed
sbchang93 <32184131+sbchang93@users.noreply.github.com>
2021-10-20 18:47:31 +0900

    Add files via upload

--- samplecode/FragmentExample-fun-s-2021/app/src/main/java/com/example/fragmentexample/view/settings_example/SettingsPreferenceActivity.java
+++ samplecode/FragmentExample-fun-s-2021/app/src/main/java/com/example/fragmentexample/view/settings_example/SettingsPreferenceActivity.java
@@ -2,12 +2,19 @@ package com.example.fragmentexample.view.settings_example;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.AlertDialog;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Toast;
 
 import com.example.fragmentexample.R;
 import com.example.fragmentexample.view.preference_screen.MySettingsFragment;
 
+import java.util.ArrayList;
+
 public class SettingsPreferenceActivity extends AppCompatActivity {
+    private final static String TAG = "SettingsPreferenceActivity";
 
     SettingsPreferenceFragment mSettingsPreferenceFragment;
 
@@ -19,4 +26,30 @@ public class SettingsPreferenceActivity extends AppCompatActivity {
         getSupportFragmentManager().beginTransaction()
                 .replace(R.id.listContainer, mSettingsPreferenceFragment).commit();
     }
+
+    public void clickUpdate(View v) {
+        AlertDialog.Builder alertdialog = new AlertDialog.Builder(this);
+        alertdialog.setTitle("FragmentExample");
+        alertdialog.setMessage("Message for Information!");
+        alertdialog.setPositiveButton(
+                "Update",
+                (dialog, which) -> {
+                    Log.e(TAG, "Update Example Apk");
+                    Toast.makeText(this, "Update Example Apk", Toast.LENGTH_SHORT).show();
+                });
+        alertdialog.setNegativeButton("Cancel",
+                (dialog, which) -> {
+                    Log.e(TAG, "Cancel Update for Example Apk");
+                    Toast.makeText(this, "Cancel Update for Example Apk", Toast.LENGTH_SHORT).show();
+                });
+
+        AlertDialog alert = alertdialog.create();
+        alert.show();
+    }
+
+    public void clickClose(View v) {
+        Log.e(TAG, "Close Update Card");
+        Toast.makeText(this, "Close Update Card", Toast.LENGTH_SHORT).show();
+    }
+
 }
