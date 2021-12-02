c3ad0285f432f7e0b914e20c2a418d3540a6f3b4
sbchang93 <32184131+sbchang93@users.noreply.github.com>
2021-12-02 19:05:51 +0900

    Add files via upload

--- samplecode/FragmentExample-fun-s-2021/app/src/main/java/com/example/fragmentexample/constants/Constants.java
+++ samplecode/FragmentExample-fun-s-2021/app/src/main/java/com/example/fragmentexample/constants/Constants.java
@@ -6,8 +6,11 @@ public class Constants {
     // 1 = i, e
     public static final int LOGGING_LEVEL = 3;
 
-    public static final String nameKey = "key_switch_preference_name";
+    public static final String cpUpdateCardKey = "key_update_card";
+
+    public static final String spNameKey = "key_switch_preference_name";
     public static final String phoneKey = "key_switch_preference_phone";
     public static final String emailKey = "key_switch_preference_email";
     public static final String memoKey = "key_switch_preference_memo";
+    public static final String updateCardKey = "key_switch_preference_update_card";
 }
