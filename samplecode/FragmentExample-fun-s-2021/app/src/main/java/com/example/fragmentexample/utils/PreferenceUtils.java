27fbe5fbd94797f0ccca4f33e6e1eff14bfe850b
sbchang93 <32184131+sbchang93@users.noreply.github.com>
2021-12-02 19:06:43 +0900

    Add files via upload

--- samplecode/FragmentExample-fun-s-2021/app/src/main/java/com/example/fragmentexample/utils/PreferenceUtils.java
+++ samplecode/FragmentExample-fun-s-2021/app/src/main/java/com/example/fragmentexample/utils/PreferenceUtils.java
@@ -20,46 +20,30 @@ public class PreferenceUtils {
         return instance;
     }
 
-    // Specific one
-    public String getFristKeyString(Context context) {
-        return (context == null) ? "0" :
-                context.getSharedPreferences(PreferenceContants.MAIN_PREFERENCE, MODE_PRIVATE).getString("key_basic_preference", "0");
-    }
-
-    public void setFirstKeyString(Context context, String id) {
-        context.getSharedPreferences(PreferenceContants.MAIN_PREFERENCE, MODE_PRIVATE).edit()
-                .putString("key_basic_preference", id).apply();
-    }
-
-    public void setBooleanWithKey(Context context, String key, boolean value) {
-        Log.d(TAG, "setBooleanWithKey " + key + "," + value);
+    public void setBoolean(Context context, String key, boolean value) {
+        Log.d(TAG, "setBoolean " + key + "," + value);
         SharedPreferences.Editor editor = context.getSharedPreferences(PreferenceContants.MAIN_PREFERENCE, MODE_PRIVATE).edit();
         editor.putBoolean(key, value);
         editor.apply();
     }
 
-    public boolean getBooleanWithKey(Context context, String key) {
+    public boolean getBoolean(Context context, String key) {
         return (context != null) &&
                 context.getSharedPreferences(PreferenceContants.MAIN_PREFERENCE, MODE_PRIVATE)
                         .getBoolean(key, false);
     }
 
-    public void setStringWithKey(Context context, String key, String value) {
+    public void setString(Context context, String key, String value) {
         Log.d(TAG, "setStringWithKey " + key + "," + value);
         SharedPreferences.Editor editor = context.getSharedPreferences(PreferenceContants.MAIN_PREFERENCE, MODE_PRIVATE).edit();
         editor.putString(key, value);
         editor.apply();
     }
 
-    public String getStringWithKey(Context context, String key) {
+    public String getString(Context context, String key) {
         if (context == null) return "";
         return context.getSharedPreferences(PreferenceContants.MAIN_PREFERENCE, MODE_PRIVATE)
                 .getString(key, "");
     }
 
-    public String getStringWithKey(Context context, String key, String defaultValue) {
-        if (context == null) return defaultValue;
-        return context.getSharedPreferences(PreferenceContants.MAIN_PREFERENCE, MODE_PRIVATE)
-                .getString(key, defaultValue);
-    }
 }
