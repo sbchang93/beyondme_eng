3d3b860295f67b0b62305f07822ee2d8f1a9e34f
sbchang93 <32184131+sbchang93@users.noreply.github.com>
2022-01-07 19:21:37 +0900

    Add files via upload

--- samplecode/SwipeUp-2022-01-03/app/src/main/java/com/example/swipeup/view/animation_example/AnimationDrawableActivity.java
+++ samplecode/SwipeUp-2022-01-03/app/src/main/java/com/example/swipeup/view/animation_example/AnimationDrawableActivity.java
@@ -5,18 +5,22 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.graphics.drawable.AnimationDrawable;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import com.example.swipeup.R;
 
 // Reference Homepage URL : https://developer.android.com/guide/topics/graphics/drawable-animation?hl=ko#java
+// Reference Homepage URL :  http://www.gisdeveloper.co.kr/?p=10261
 
 public class AnimationDrawableActivity extends AppCompatActivity {
 
     AnimationDrawable weatherAnimation;
     AnimationDrawable mainBgAnimation;
     ImageView weatherImage;
+    Button myButtonView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,6 +49,39 @@ public class AnimationDrawableActivity extends AppCompatActivity {
                 mainBgAnimation.start();
             }
         });
+
+        myButtonView = findViewById(R.id.my_button_view);
+        myButtonView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                myButtonView.animate()
+                        .scaleX(5.0f)
+                        .scaleY(5.0f)
+                        .alpha(0.0f)
+                        .translationX(400.0f)
+                        .translationY(400.0f)
+                        .rotation(360.0f)
+                        .setDuration(1000)
+                        .withStartAction(new Runnable() {
+                            public void run() {
+                                Toast.makeText(AnimationDrawableActivity.this, "애니메이션 시작", Toast.LENGTH_SHORT).show();
+                            }
+                        })
+                        .withEndAction(new Runnable() {
+                            public void run() {
+                                myButtonView.animate()
+                                        .scaleX(1.0f).scaleY(1.0f)
+                                        .alpha(1.0f)
+                                        .x(0.0f).y(0.0f)
+                                        .rotation(0.0f);
+                                Toast.makeText(AnimationDrawableActivity.this, "완료", Toast.LENGTH_SHORT).show();
+                            }
+                        });
+            }
+        });
+
+
+
     }
 
     @Override
