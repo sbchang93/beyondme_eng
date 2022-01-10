3f4e9d3fd355fe935ec53533e17668e693910ce1
sbchang93 <32184131+sbchang93@users.noreply.github.com>
2022-01-10 20:20:13 +0900

    Add files via upload

--- samplecode/SwipeUp-2022-01-03/app/src/main/java/com/example/swipeup/view/animation_example/ViewPropertyAnimatorActivity.java
+++ samplecode/SwipeUp-2022-01-03/app/src/main/java/com/example/swipeup/view/animation_example/ViewPropertyAnimatorActivity.java
@@ -15,6 +15,7 @@ import android.widget.Toast;
 import com.example.swipeup.R;
 
 // Reference Home URL : https://ichi.pro/ko/eotteon-android-animatorga-sayonghagie-jeoghabhangayo-227415534195479
+//https://m.blog.naver.com/PostView.naver?isHttpsRedirect=true&blogId=tkddlf4209&logNo=221638722158
 
 public class ViewPropertyAnimatorActivity extends AppCompatActivity {
     private static final String TAG = "ViewPropertyAnimatorActivity";
@@ -27,7 +28,7 @@ public class ViewPropertyAnimatorActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_property_animator);
 
-        textView1 = findViewById(R.id.anim_property_text1);
+        textView1 = findViewById(R.id.text1);
         //iewPropertyAnimator animationTextView1 = textView1.animate();
         textView1.animate().rotationX(3600f)
                 .setDuration(5000)
@@ -42,7 +43,7 @@ public class ViewPropertyAnimatorActivity extends AppCompatActivity {
                     }
                 });
 
-        textView2 = findViewById(R.id.anim_property_text2);
+        textView2 = findViewById(R.id.text2);
         textView2.animate()
                 .rotationX(3600f)
                 .setDuration(3000)
@@ -61,6 +62,65 @@ public class ViewPropertyAnimatorActivity extends AppCompatActivity {
                         animationTextView2.start();
                     }
                 });
+
+
+        // https://m.blog.naver.com/PostView.naver?isHttpsRedirect=true&blogId=tkddlf4209&logNo=221638722158
+        // Increase the height(Y) until it reaches 300f. ( Repeat infinitely. )
+        ImageView imageBear1 = findViewById(R.id.bear1);
+        float translation_value = 300f;
+        ObjectAnimator anim1 = ObjectAnimator.ofFloat(imageBear1, "translationY", translation_value);
+        anim1.setDuration(5000); // duration 5 seconds
+        anim1.setRepeatCount(ValueAnimator.INFINITE);
+        //anim1.setRepeatMode(ValueAnimator.REVERSE);
+        //anim.setInterpolator(new CycleInterpolator(1));
+        anim1.start();
+
+        // 0 ~ 300 : <=== animation.getAnimatedValue()
+        ImageView imageCat = findViewById(R.id.cat);
+        int height_value = 300;
+        ValueAnimator anim2 = ValueAnimator.ofInt(0, height_value);
+        anim2.setDuration(5000); // duration 5 seconds
+        anim2.setRepeatCount(ValueAnimator.INFINITE);
+        anim2.setRepeatMode(ValueAnimator.REVERSE);
+        anim2.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                Integer value = (Integer) animation.getAnimatedValue();  // 0 ~ 300
+                imageCat.getLayoutParams().height = value.intValue();
+                imageCat.requestLayout();
+            }
+        });
+        anim2.start();
+
+
+        //https://m.blog.naver.com/PostView.naver?isHttpsRedirect=true&blogId=tkddlf4209&logNo=221638722158
+        View redScanLine = findViewById(R.id.scan_line);
+        float translation_value3 = 300f;
+        ObjectAnimator anim3 = ObjectAnimator.ofFloat(redScanLine, "translationY", translation_value3);
+
+        anim3.setDuration(5000); // duration 5 seconds
+        anim3.setRepeatCount(ValueAnimator.INFINITE);
+        anim3.setRepeatMode(ValueAnimator.REVERSE);
+        //anim4.setInterpolator(new CycleInterpolator(1));
+        anim3.start();
+
+        View greenScan = findViewById(R.id.scan_fill);
+        int height_value4 = 300;
+        ValueAnimator anim4 = ValueAnimator.ofInt(0, height_value4);
+        anim4.setDuration(5000); // duration 5 seconds
+        anim4.setRepeatCount(ValueAnimator.INFINITE);
+        anim4.setRepeatMode(ValueAnimator.REVERSE);
+        anim4.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation) {
+                Integer value = (Integer) animation.getAnimatedValue();  // 0 ~ 300
+                greenScan.getLayoutParams().height = value.intValue();
+                greenScan.requestLayout();
+            }
+        });
+        anim4.start();
+
+
 
 
     }
