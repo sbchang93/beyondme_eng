686be2c6cf905feba850f73a3a750cf4dbb2b20a
sbchang93 <32184131+sbchang93@users.noreply.github.com>
2022-01-27 20:43:55 +0900

    Add files via upload

--- samplecode/SwipeUp-2022-01-03/app/src/main/java/com/example/swipeup/view/android_hero_samples/customview/DragViewGroup.java
+++ samplecode/SwipeUp-2022-01-03/app/src/main/java/com/example/swipeup/view/android_hero_samples/customview/DragViewGroup.java
@@ -34,7 +34,6 @@ public class DragViewGroup extends FrameLayout {
         initView();
     }
 
-    //加载完布局文件后调用
     @Override
     protected void onFinishInflate() {
         super.onFinishInflate();
@@ -55,7 +54,6 @@ public class DragViewGroup extends FrameLayout {
 
     @Override
     public boolean onTouchEvent(MotionEvent event) {
-        //将触摸事件传递给ViewDragHelper,此操作必不可少
         mViewDragHelper.processTouchEvent(event);
         return true;
     }
@@ -67,57 +65,46 @@ public class DragViewGroup extends FrameLayout {
     private ViewDragHelper.Callback callback =
             new ViewDragHelper.Callback() {
 
-                // 何时开始检测触摸事件
                 @Override
                 public boolean tryCaptureView(View child, int pointerId) {
                     //如果当前触摸的child是mMainView时开始检测
                     return mMainView == child;
                 }
 
-                // 触摸到View后回调
                 @Override
                 public void onViewCaptured(View capturedChild,
                                            int activePointerId) {
                     super.onViewCaptured(capturedChild, activePointerId);
                 }
 
-                // 当拖拽状态改变，比如idle，dragging
                 @Override
                 public void onViewDragStateChanged(int state) {
                     super.onViewDragStateChanged(state);
                 }
 
-                // 当位置改变的时候调用,常用与滑动时更改scale等
                 @Override
                 public void onViewPositionChanged(View changedView,
                                                   int left, int top, int dx, int dy) {
                     super.onViewPositionChanged(changedView, left, top, dx, dy);
                 }
 
-                // 处理垂直滑动
                 @Override
                 public int clampViewPositionVertical(View child, int top, int dy) {
                     return 0;
                 }
 
-                // 处理水平滑动
                 @Override
                 public int clampViewPositionHorizontal(View child, int left, int dx) {
                     return left;
                 }
 
-                // 拖动结束后调用
                 @Override
                 public void onViewReleased(View releasedChild, float xvel, float yvel) {
                     super.onViewReleased(releasedChild, xvel, yvel);
-                    //手指抬起后缓慢移动到指定位置
                     if (mMainView.getLeft() < 500) {
-                        //关闭菜单
-                        //相当于Scroller的startScroll方法
                         mViewDragHelper.smoothSlideViewTo(mMainView, 0, 0);
                         ViewCompat.postInvalidateOnAnimation(DragViewGroup.this);
                     } else {
-                        //打开菜单
                         mViewDragHelper.smoothSlideViewTo(mMainView, 300, 0);
                         ViewCompat.postInvalidateOnAnimation(DragViewGroup.this);
                     }
