590f1eed9798fd07b371376a34e7ff1c205eec67
sbchang93 <32184131+sbchang93@users.noreply.github.com>
2022-02-11 17:49:11 +0900

    Add files via upload

---
+++ samplecode/SwipeUp-2022-01-03/app/src/main/java/com/example/swipeup/view_utils/UIHelper.java
@@ -0,0 +1,136 @@
+package com.example.swipeup.view_utils;
+
+import android.annotation.TargetApi;
+import android.app.Activity;
+import android.content.Context;
+import android.content.res.Configuration;
+import android.content.res.Resources;
+import android.os.Build;
+import android.view.ViewConfiguration;
+
+import java.lang.reflect.Method;
+
+//Reference Home URL : https://github.com/GavinAndre/AndroidHeroSamples
+//Reference Home URL : https://programmer.help/blogs/android-scroll-analysis-3-the-ultimate-way-to-slide-viewdraghelper.html
+
+public class UIHelper {
+
+    public static int dipToPx(Context context, float dip) {
+        return (int) (dip * context.getResources().getDisplayMetrics().density + 0.5f);
+    }
+
+    public static int pxToDip(Context context, float pxValue) {
+        final float scale = context.getResources().getDisplayMetrics().density;
+        return (int) (pxValue / scale + 0.5f);
+    }
+
+    public static int sp2px(Context context, float spValue) {
+        final float fontScale = context.getResources().getDisplayMetrics().scaledDensity;
+        return (int) (spValue * fontScale + 0.5f);
+    }
+
+    public static int px2sp(Context context, float pxValue) {
+        final float fontScale = context.getResources().getDisplayMetrics().scaledDensity;
+        return (int) (pxValue / fontScale + 0.5f);
+    }
+
+    public static int getScreenPixWidth(Context context) {
+        return context.getResources().getDisplayMetrics().widthPixels;
+    }
+
+    public static int getScreenPixHeight(Context context) {
+        return context.getResources().getDisplayMetrics().heightPixels;
+    }
+
+    public static int getStatusHeight(Context context) {
+        int result = 0;
+        int resourceId = context.getResources().getIdentifier("status_bar_height", "dimen", "包名");//包名:com.example.application
+        if (resourceId > 0) {
+            result = context.getResources().getDimensionPixelSize(resourceId);
+        }
+        return result;
+    }
+    public static int getStatusHeightFromAndroid(Context context) {
+        int result = 0;
+        int resourceId = context.getResources().getIdentifier("status_bar_height", "dimen", "android");
+        if (resourceId > 0) {
+            result = context.getResources().getDimensionPixelSize(resourceId);
+        }
+        return result;
+    }
+
+    public static int getTitleHeight(Context context) {
+        int result = 0;
+        int resourceId = context.getResources().getIdentifier("title_bar_height", "dimen", "包名");//包名:com.example.application
+        if (resourceId > 0) {
+            result = context.getResources().getDimensionPixelSize(resourceId);
+        }
+        return result;
+    }
+
+    @TargetApi(14)
+    public static int getNavigationBarHeight(Activity activity) {
+        return getNavigationBarHeight((Context) activity);
+    }
+
+    @TargetApi(14)
+    private static int getNavigationBarHeight(Context context) {
+        Resources res = context.getResources();
+        int result = 0;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.ICE_CREAM_SANDWICH) {
+            if (hasNavBar(context)) {
+                String key;
+                boolean mInPortrait = (res.getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT);
+                if (mInPortrait) {
+                    key = "navigation_bar_height";
+                } else {
+                    key = "navigation_bar_height_landscape";
+                }
+                return getInternalDimensionSize(res, key);
+            }
+        }
+        return result;
+    }
+
+    @TargetApi(14)
+    private static boolean hasNavBar(Context context) {
+        Resources res = context.getResources();
+        int resourceId = res.getIdentifier("config_showNavigationBar", "bool", "android");
+        if (resourceId != 0) {
+            boolean hasNav = res.getBoolean(resourceId);
+            if ("1".equals(getNavBarOverride())) {
+                hasNav = false;
+            } else if ("0".equals(getNavBarOverride())) {
+                hasNav = true;
+            }
+            return hasNav;
+        } else {
+            return !ViewConfiguration.get(context).hasPermanentMenuKey();
+        }
+    }
+
+    @TargetApi(19)
+    private static String getNavBarOverride() {
+        String isNavBarOverride = null;
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+            try {
+                Class c = Class.forName("android.os.SystemProperties");
+                Method m = c.getDeclaredMethod("get", String.class);
+                m.setAccessible(true);
+                isNavBarOverride = (String) m.invoke(null, "qemu.hw.mainkeys");
+            } catch (Throwable e) {
+                isNavBarOverride = null;
+            }
+        }
+        return isNavBarOverride;
+    }
+
+    private static int getInternalDimensionSize(Resources res, String key) {
+        int result = 0;
+        int resourceId = res.getIdentifier(key, "dimen", "android");
+        if (resourceId > 0) {
+            result = res.getDimensionPixelSize(resourceId);
+        }
+        return result;
+    }
+}
