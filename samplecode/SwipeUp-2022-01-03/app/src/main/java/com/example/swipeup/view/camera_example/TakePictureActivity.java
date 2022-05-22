267cf2a82bea6b17f937f074dfe322c0b31e4740
sbchang93 <32184131+sbchang93@users.noreply.github.com>
2022-05-22 17:21:26 +0900

    Add files via upload

--- samplecode/SwipeUp-2022-01-03/app/src/main/java/com/example/swipeup/view/camera_example/TakePictureActivity.java
+++ samplecode/SwipeUp-2022-01-03/app/src/main/java/com/example/swipeup/view/camera_example/TakePictureActivity.java
@@ -3,6 +3,7 @@ package com.example.swipeup.view.camera_example;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
+import androidx.core.content.FileProvider;
 
 import android.Manifest;
 import android.content.Intent;
@@ -17,6 +18,7 @@ import android.view.View;
 import android.widget.ImageView;
 import android.widget.Toast;
 
+import com.example.swipeup.BuildConfig;
 import com.example.swipeup.R;
 
 import java.io.File;
@@ -24,22 +26,36 @@ import java.io.File;
 //Reference Home URL : https://stackoverflow.com/questions/29777098/attempt-to-invoke-virtual-method-void-android-widget-imageview-setimagebitmapa
 //Reference Home URL : https://developer.android.com/training/camera/photobasics?hl=ko#java
 //Reference Home URL : https://bottlecok.tistory.com/entry/%EC%95%88%EB%93%9C%EB%A1%9C%EC%9D%B4%EB%93%9C-%EA%B6%8C%ED%95%9C%EC%9A%94%EC%B2%AD-%EA%B6%8C%ED%95%9C%EC%84%A4%EC%A0%95-%ED%8D%BC%EB%AF%B8%EC%85%98%EC%B2%B4%ED%81%AC
+
+// Caused by: android.os.FileUriExposedException: file:///storage/emulated/0/Download/image.jpg exposed beyond app through ClipData.Item.getUri(
+// => Trouble shooting => https://rlg1133.tistory.com/53
+//                     => https://mond-al.github.io/cachefile-share
+
 public class TakePictureActivity extends AppCompatActivity {
     private final static String TAG = "TakePictureActivity";
 
-    private final int MY_PERMISSIONS_REQUEST_CAMERA=1001;
+    private final int MY_PERMISSIONS_REQUEST_CAMERA = 1001;
 
     public static final String ExternalDownloadsFolderPath = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS).getAbsolutePath();
 
     static final int REQUEST_IMAGE_CAPTURE = 1;
+    static final int REQUEST_IMAGE_CAPTURE_WITH_BOTTON_2 = 2;
+    static final int REQUEST_IMAGE_CAPTURE_WITH_BOTTON_3 = 3;
+    static final int REQUEST_IMAGE_CAPTURE_WITH_BOTTON_4 = 4;
+
     private ImageView imageView;
+    private ImageView imageView2;
+    private ImageView imageView3;
+    private ImageView imageView4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_take_picture);
         imageView = findViewById(R.id.imageView);
-
+        imageView2 = findViewById(R.id.imageView2);
+        imageView3 = findViewById(R.id.imageView3);
+        imageView4 = findViewById(R.id.imageView4);
 
         if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
             if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.CAMERA)) {
@@ -48,23 +64,9 @@ public class TakePictureActivity extends AppCompatActivity {
             }
         }
 
-        //        openCamera();
+        // openCamera();
     }
 
-    @Override
-    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
-        switch (requestCode) {
-            case MY_PERMISSIONS_REQUEST_CAMERA: {
-                // If request is cancelled, the result arrays are empty.
-                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                    Toast.makeText(this, "승인이 허가되어 있습니다.", Toast.LENGTH_LONG).show();
-                } else {
-                    Toast.makeText(this, "아직 승인받지 않았습니다.", Toast.LENGTH_LONG).show();
-                }
-                return;
-            }
-        }
-    }
 
     public void openCamera() {
         Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
@@ -79,12 +81,22 @@ public class TakePictureActivity extends AppCompatActivity {
         //Check that request code matches ours:
         if (requestCode == REQUEST_IMAGE_CAPTURE) {
             //Get our saved file into a bitmap object:
-//            File file = new File(Environment.getExternalStorageDirectory() + File.separator + "image.jpg");
-//            File file = new File(ExternalDownloadsFolderPath + File.separator + "image.jpg");
-//            Bitmap image = decodeSampledBitmapFromFile(file.getAbsolutePath(), 1000, 700);
+            //File file = new File(Environment.getExternalStorageDirectory() + File.separator + "image.jpg");
+            File file = new File(ExternalDownloadsFolderPath + File.separator + "image.jpg");
+            Bitmap imageBitmap = decodeSampledBitmapFromFile(file.getAbsolutePath(), 1000, 700);
+            imageView.setImageBitmap(imageBitmap);
+        } else if (requestCode == REQUEST_IMAGE_CAPTURE_WITH_BOTTON_2) {
             Bundle extras = data.getExtras();
             Bitmap imageBitmap = (Bitmap) extras.get("data");
-            imageView.setImageBitmap(imageBitmap);
+            imageView2.setImageBitmap(imageBitmap);
+        } else if (requestCode == REQUEST_IMAGE_CAPTURE_WITH_BOTTON_3) {
+            File file = new File(ExternalDownloadsFolderPath + File.separator + "image.jpg");
+            Bitmap imageBitmap = decodeSampledBitmapFromFile(file.getAbsolutePath(), 1000, 700);
+            imageView3.setImageBitmap(imageBitmap);
+        } else if (requestCode == REQUEST_IMAGE_CAPTURE_WITH_BOTTON_4) {
+            Bundle extras = data.getExtras();
+            Bitmap imageBitmap = (Bitmap) extras.get("data");
+            imageView4.setImageBitmap(imageBitmap);
         }
     }
 
@@ -129,15 +141,59 @@ public class TakePictureActivity extends AppCompatActivity {
     }
 
     public void capture_btn(View v) {
-
         Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
-            //File file = new File(Environment.getExternalStorageDirectory() + File.separator + "image.jpg");
-//            File file = new File(ExternalDownloadsFolderPath + File.separator + "image.jpg");
-//            takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(file));
-            startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
-        } else {
-            Toast.makeText(this, "Camera permission not granted", Toast.LENGTH_SHORT).show();
+        File file = new File(ExternalDownloadsFolderPath + File.separator + "image.jpg");
+        Uri photoUri = FileProvider.getUriForFile(this, "com.example.swipeup.fileprovider", file);
+        takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoUri);
+        startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+
+        // File file = new File(uri.getPath());
+        //Uri photoUri = FileProvider.getUriForFile(this, BuildConfig.APPLICATION_ID+".fileprovider", file);
+
+        // Caused by: android.os.FileUriExposedException: file:///storage/emulated/0/Download/image.jpg exposed beyond app through ClipData.Item.getUri(
+        // => Use FileProvider class
+        //File file = new File(Environment.getExternalStorageDirectory() + File.separator + "image.jpg");
+        //File file = new File(ExternalDownloadsFolderPath + File.separator + "image.jpg");
+        //takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(file));
+    }
+
+    public void capture_btn2(View v) {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE_WITH_BOTTON_2);
+    }
+
+    public void capture_btn3(View v) {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        //todo: (NeedToFix) When we receive image data from Camera, There is an problem in onActivityResult(..) function.
+        // => Can not get the capture image infromation from "image.jpg" by saved by Camera App.
+        File file = new File(ExternalDownloadsFolderPath + File.separator + "image.jpg");
+        Uri photoUri = FileProvider.getUriForFile(this, "com.example.swipeup.fileprovider", file);
+        takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoUri);
+        startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE_WITH_BOTTON_3);
+    }
+
+    public void capture_btn4(View v) {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        //todo: (NeedToFix) When we receive image data from Camera, There is an problem in onActivityResult(..) function.
+        // => Caused by: java.lang.NullPointerException: Attempt to invoke virtual method 'android.os.Bundle android.content.Intent.getExtras()' on a null object reference0
+        File file = new File(ExternalDownloadsFolderPath + File.separator + "image.jpg");
+        Uri photoUri = FileProvider.getUriForFile(this, "com.example.swipeup.fileprovider", file);
+        takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoUri);
+        startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE_WITH_BOTTON_4);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case MY_PERMISSIONS_REQUEST_CAMERA: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(this, "승인이 허가되어 있습니다.", Toast.LENGTH_LONG).show();
+                } else {
+                    Toast.makeText(this, "아직 승인받지 않았습니다.", Toast.LENGTH_LONG).show();
+                }
+                return;
+            }
         }
     }
 }
