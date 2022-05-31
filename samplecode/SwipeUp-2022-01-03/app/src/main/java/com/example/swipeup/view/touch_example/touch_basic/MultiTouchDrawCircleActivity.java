6f935b75e2ddaa7820e2cfbd371652ab944904a3
sbchang93 <32184131+sbchang93@users.noreply.github.com>
2022-06-01 02:05:19 +0900

    Add files via upload

--- samplecode/SwipeUp-2022-01-03/app/src/main/java/com/example/swipeup/view/touch_example/touch_basic/MultiTouchDrawCircleActivity.java
+++ samplecode/SwipeUp-2022-01-03/app/src/main/java/com/example/swipeup/view/touch_example/touch_basic/MultiTouchDrawCircleActivity.java
@@ -34,6 +34,7 @@ public class MultiTouchDrawCircleActivity extends AppCompatActivity {
 
     class MyMultiTouchView extends View {
         private Paint paint;
+        private Paint whitePaint;
 
         final int MAX_POINTS = 10;
 
@@ -48,6 +49,10 @@ public class MultiTouchDrawCircleActivity extends AppCompatActivity {
             paint = new Paint(Paint.ANTI_ALIAS_FLAG);
             paint.setColor(Color.MAGENTA);
             paint.setStyle(Paint.Style.FILL_AND_STROKE);
+
+            whitePaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+            whitePaint.setColor(Color.WHITE);
+            whitePaint.setStyle(Paint.Style.FILL_AND_STROKE);
         }
 
         @Override
@@ -57,6 +62,8 @@ public class MultiTouchDrawCircleActivity extends AppCompatActivity {
             for (int i = 0; i < MAX_POINTS; i++) {
                 if (touching[i]) {
                     canvas.drawCircle(x[i], y[i], 30, paint);
+                } else {
+                    canvas.drawCircle(x[i], y[i], 30, whitePaint);
                 }
             }
         }
@@ -72,30 +79,35 @@ public class MultiTouchDrawCircleActivity extends AppCompatActivity {
             switch (action) {
                 case MotionEvent.ACTION_DOWN:
                 case MotionEvent.ACTION_POINTER_DOWN:
-                    for (int i = 0; i < pointer_count; i++) {
-                        Log.i(TAG, "id : " + i);
-                        x[i] = event.getX(i);
-                        y[i] = event.getY(i);
-                        mPointerId[i] = event.getPointerId(i);
-                        touching[activePointerId] = true;
-                    }
+                    Log.i(TAG, "ACTION_DOWN, ACTION_POINTER_DOWN - id (activeIndex) : " + activeIndex);
+                    x[activeIndex] = event.getX(activeIndex);
+                    y[activeIndex] = event.getY(activeIndex);
+                    mPointerId[activeIndex] = event.getPointerId(activeIndex);
+                    touching[mPointerId[activeIndex]] = true;
                     break;
 
                 case MotionEvent.ACTION_MOVE:
+                    // 모든 ActiveIndex들을 모두 처리해 줌. ( i : active index )
                     for (int i = 0; i < pointer_count; i++) {
+                        Log.i(TAG, "ACTION_MOVE - id (activeIndex) : " + i + ", event.getPointerId(i)" + event.getPointerId(i));
                         x[i] = event.getX(i);
                         y[i] = event.getY(i);
                         mPointerId[i] = event.getPointerId(i);
-                        touching[i] = true;
+                        touching[mPointerId[i]] = true;
                     }
                     break;
 
                 case MotionEvent.ACTION_POINTER_UP:
                 case MotionEvent.ACTION_UP:
                 case MotionEvent.ACTION_CANCEL:
+
+                    activeIndex = event.getActionIndex();
+                    activePointerId = event.getPointerId(activeIndex);
+                    Log.i(TAG, "ACTION_POINTER_UP, ACTION_UP - id (activeIndex) : " + activeIndex + ", activePointerId : " + activePointerId +", pointer_count : " + pointer_count);
                     for (int i = 0; i < pointer_count; i++) {
                         if (i == activePointerId) {
-                            touching[i] = false;
+                            Log.i(TAG, "ACTION_POINTER_UP, ACTION_UP - touching[activePointerId] = false , activePointerId  : " + activePointerId);
+                            touching[activePointerId] = false;
                             break;
                         }
                     }
@@ -108,7 +120,63 @@ public class MultiTouchDrawCircleActivity extends AppCompatActivity {
         }
     }
 
+//=======================================================================================
+//=======================================================================================
+// Active Index 만 사용한 코드 : 뭔가 2% 부족하다. - ActivePointerId를 사용해야 할 것 같다.
+// 손가락을 4개 정도 터치한 뒤에, 하나씩 손을 떼면서 테스트해 보면 사라지지 않는 터치 동그라미가 보인다.
+//=======================================================================================
+//=======================================================================================
+//        @Override
+//        public boolean onTouchEvent(MotionEvent event) {
+//            int pointer_count = event.getPointerCount();
+//            int activeIndex = event.getActionIndex();
+//            int activePointerId = event.getPointerId(activeIndex);
+//            Log.i(TAG, "activePointerId : " + activePointerId);
+//            int action = event.getActionMasked();   // for Multi Pointers
+//
+//            switch (action) {
+//                case MotionEvent.ACTION_DOWN:
+//                case MotionEvent.ACTION_POINTER_DOWN:
+//                        Log.i(TAG, "id : " + activeIndex);
+//                        x[activeIndex] = event.getX(activeIndex);
+//                        y[activeIndex] = event.getY(activeIndex);
+//                        mPointerId[activeIndex] = event.getPointerId(activeIndex);
+//                        touching[activeIndex] = true;
+//                    break;
+//
+//                case MotionEvent.ACTION_MOVE:
+//                    for (int i = 0; i < pointer_count; i++) {
+//                        x[i] = event.getX(i);
+//                        y[i] = event.getY(i);
+//                        mPointerId[i] = event.getPointerId(i);
+//                        touching[i] = true;
+//                    }
+//                    break;
+//
+//                case MotionEvent.ACTION_POINTER_UP:
+//                case MotionEvent.ACTION_UP:
+//                case MotionEvent.ACTION_CANCEL:
+//                    for (int i = 0; i < pointer_count; i++) {
+//                        if (i == activeIndex) {
+//                            touching[i] = false;
+//                            break;
+//                        }
+//                    }
+//                    break;
+//            }
+//
+//            invalidate();
+//
+//            return true;
+//        }
+//    }
+
+
+//=======================================================================================
+//=======================================================================================
 //    Original Codes ( 원본 코드 ) - 멀티 터치에 대해 Circle을 못 그려줌.
+//=======================================================================================
+//=======================================================================================
 //    @Override
 //    public boolean onTouchEvent(MotionEvent event) {
 //        int index = event.getActionIndex();
