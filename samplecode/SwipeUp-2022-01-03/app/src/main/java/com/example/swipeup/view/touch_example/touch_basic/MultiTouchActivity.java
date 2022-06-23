dd6e2a4b4601aedcbf09b750d1ada9e8fef961fb
sbchang93 <32184131+sbchang93@users.noreply.github.com>
2022-06-23 20:53:42 +0900

    Add files via upload

--- samplecode/SwipeUp-2022-01-03/app/src/main/java/com/example/swipeup/view/touch_example/touch_basic/MultiTouchActivity.java
+++ samplecode/SwipeUp-2022-01-03/app/src/main/java/com/example/swipeup/view/touch_example/touch_basic/MultiTouchActivity.java
@@ -12,9 +12,10 @@ import com.example.swipeup.R;
 
 public class MultiTouchActivity extends AppCompatActivity {
 
-    int id[] = new int[3];
-    int x[] = new int[3];
-    int y[] = new int[3];
+    final int MAX_POINTER_COUNTER = 4;
+    int id[] = new int[10]; // new int[3];
+    int x[] = new int[10]; //new int[3];
+    int y[] = new int[10]; //new int[3];
     String result;
 
     TextView tv1;
@@ -33,7 +34,8 @@ public class MultiTouchActivity extends AppCompatActivity {
     @Override
     public boolean onTouchEvent(MotionEvent event) {
         int pointer_count = event.getPointerCount(); //현재 터치 발생한 포인트 수를 얻는다.
-        if(pointer_count > 3) pointer_count = 3; //4개 이상의 포인트를 터치했더라도 3개까지만 처리를 한다.
+        if(pointer_count > MAX_POINTER_COUNTER) pointer_count = MAX_POINTER_COUNTER;
+        //if(pointer_count > 3) pointer_count = 3; //4개 이상의 포인트를 터치했더라도 3개까지만 처리를 한다.
 
         switch(event.getAction() & MotionEvent.ACTION_MASK) {
             case MotionEvent.ACTION_DOWN: //한 개 포인트에 대한 DOWN을 얻을 때.
@@ -47,6 +49,7 @@ public class MultiTouchActivity extends AppCompatActivity {
                 break;
 
             case MotionEvent.ACTION_POINTER_DOWN: //두 개 이상의 포인트에 대한 DOWN을 얻을 때.
+                tv2.setText("");
                 result = "Multi-Touch :\nACTION_POINTER_DOWN: \n";
                 for(int i = 0; i < pointer_count; i++) {
                     id[i] = event.getPointerId(i); //터치한 순간부터 부여되는 포인트 고유번호.
