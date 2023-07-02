18098802ce191f5f37b3ee2cc8990d1984b6e15f
sbchang93 <32184131+sbchang93@users.noreply.github.com>
2023-07-02 22:21:18 +0900

    Add files via upload

--- samplecode/TextPlayer/TextPlayer-003-Next01/app/src/main/java/com/flower/textplayer/MainActivity.java
+++ samplecode/TextPlayer/TextPlayer-003-Next01/app/src/main/java/com/flower/textplayer/MainActivity.java
@@ -16,12 +16,38 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.Locale;
+import java.util.concurrent.TimeUnit;
+
+import io.reactivex.Observable;
+import io.reactivex.android.schedulers.AndroidSchedulers;
 
 //Reference Homepage URL : https://zion830.tistory.com/51
 //Reference Homepage URL : https://gist.github.com/zion830/d045325ec84b7cb1a545f31f65ff966c
 //Reference Homepage URL : https://channelg.co.kr/entry/%EC%A7%A7%EC%9D%80-%EC%98%81%EC%96%B4-%EB%AA%85%EC%96%B8-%EB%AA%A8%EC%9D%8C%EC%A7%91-75%EA%B0%9C-%EB%A0%88%ED%84%B0%EB%A7%81-%EB%AC%B8%EA%B5%AC-%EC%B6%94%EC%B2%9C
 
+//// Sample Code
+//private static final int DEFAULT_VALUE_OFFSET = 0;
+//public static final int VERSION_CODE = 1;
+//private static int nextId = 0;
+//public static final boolean DEBUG = Boolean.parseBoolean("true");
+//public static final String APPLICATION_ID = "com.flower.textplayer";
+//public static final String BUILD_TYPE = "debug";
+//public static final String VERSION_NAME = "1.0";
+//private final static String TAG = "MainActivity";
+//private static final String TAG = CanvasSaveProxy.class.getSimpleName();
+//private static final String METHOD_NAME = "save";
+//private static final String FIELD_NAME = "CLIP_SAVE_FLAG";
+//private static final int MY_PERMISSION_CAMERA = 1111;
+//private static final int REQUEST_TAKE_PHOTO = 2222;
+//private static final int REQUEST_TAKE_ALBUM = 3333;
+//private static final int REQUEST_IMAGE_CROP = 4444;
+
+
 public class MainActivity extends AppCompatActivity implements TextPlayer, View.OnClickListener {
+    private final static String TAG = "MainActivity-TextPlayer";
+    private static final int PLAY_1_TIME = 1;
+    private static final int PLAY_N_TIME = 2;
+    private static final int PLAY_FOR_5_MINUTES = 3;
 
     private final Bundle params = new Bundle();
     private final BackgroundColorSpan colorSpan = new BackgroundColorSpan(Color.YELLOW);
@@ -41,7 +67,9 @@ public class MainActivity extends AppCompatActivity implements TextPlayer, View.
     private int standbyIndex = 0;
     private int lastPlayIndex = 0;
     private boolean isLanguageToggle = false; // false => English Language
-    private boolean isRepeatSentence = false;
+    private int mRepeatMode = PLAY_1_TIME;
+    private boolean isRunningFor5Minutes = false;
+    //private boolean isRepeatSentence = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -100,7 +128,7 @@ public class MainActivity extends AppCompatActivity implements TextPlayer, View.
             @Override
             public void onDone(String s) {
                 clearAll();
-                if(isRepeatSentence) {
+                if((mRepeatMode == PLAY_N_TIME) || (mRepeatMode == PLAY_FOR_5_MINUTES && isRunningFor5Minutes == true)) {
                     runOnUiThread(new Runnable() {
                         @Override
                         public void run() {
@@ -192,11 +220,18 @@ public class MainActivity extends AppCompatActivity implements TextPlayer, View.
                 break;
 
             case R.id.btn_repeat:
-                isRepeatSentence = !isRepeatSentence;
-                if (isRepeatSentence == false) {
-                    repeatBtn.setText("1");
-                } else {
+                if (mRepeatMode  == PLAY_1_TIME) {
                     repeatBtn.setText("N");
+                    mRepeatMode = PLAY_N_TIME;
+                } else if (mRepeatMode  == PLAY_N_TIME) {
+                    repeatBtn.setText("5M");
+                    mRepeatMode = PLAY_FOR_5_MINUTES;
+                } else if (mRepeatMode  == PLAY_FOR_5_MINUTES) {
+                    repeatBtn.setText("1");
+                    mRepeatMode = PLAY_1_TIME;
+                }  else { // This syntax is defensive code for Exceptional Cases. But it doesn't happen.
+                    repeatBtn.setText("1");
+                    mRepeatMode = PLAY_1_TIME;
                 }
                 break;
 
@@ -214,6 +249,15 @@ public class MainActivity extends AppCompatActivity implements TextPlayer, View.
             startSpeak(content.substring(standbyIndex));
         }
         playState = PlayState.PLAY;
+
+        if(mRepeatMode == PLAY_FOR_5_MINUTES) {
+            isRunningFor5Minutes = true;
+            Observable.timer(5, TimeUnit.MINUTES)
+                    //.observeOn(AndroidSchedulers.mainThread())
+                    .subscribe(s -> {
+                        isRunningFor5Minutes = false;
+                    }, t -> t.getMessage());
+        }
     }
 
     @Override
